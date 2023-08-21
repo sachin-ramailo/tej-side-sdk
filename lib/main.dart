8a1676a85e87e681e4822aecda70de66ffe22b88
sachin <sachinsabbarwal@gmail.com>
2023-08-21 15:06:50 +0530

    showing mnemonic on screen

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  late String mnemonic;
+  @override
+  void initState() {
+    super.initState();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -115,46 +120,56 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextButton(
-              onPressed: () {
-                testFunc();
-              },
-              child: const Text(
-                "Sasdf",
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                AccountsUtil.getInstance().createAccount();
-              },
-              child: const Text(
-                "Create wallet",
-              ),
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: FutureBuilder(
+          future: AccountsUtil.getInstance().getAccountPhrase(),
+          builder: (context, snapshot) {
+            if(snapshot.hasData){
+              return Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                //
+                // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+                // action in the IDE, or press "p" in the console), to see the
+                // wireframe for each widget.
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(snapshot.data!,textAlign: TextAlign.center,),
+                  TextButton(
+                    onPressed: () {
+                      testFunc();
+                    },
+                    child: const Text(
+                      "Sasdf",
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      AccountsUtil.getInstance().createAccount();
+                    },
+                    child: const Text(
+                      "Create wallet",
+                    ),
+                  ),
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              );
+            }
+            return const Text("Loading...");
+
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
